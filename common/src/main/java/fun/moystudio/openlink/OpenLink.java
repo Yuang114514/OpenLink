ef04f4ade0a0042e8518f3d53feea23cac00f4fc
Terry-mcgit <13884967603@163.com>
2024-09-28 14:21:48 +0800

    Testing...

--- common/src/main/java/fun/moystudio/openlink/OpenLink.java
+++ common/src/main/java/fun/moystudio/openlink/OpenLink.java
@@ -1,9 +1,22 @@
 package fun.moystudio.openlink;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public final class OpenLink {
     public static final String MOD_ID = "openlink";
+    public static final Logger LOGGER = LogManager.getLogger("OpenLink");
 
     public static void init() {
-        // Write common init code here.
+        LOGGER.info("Initializing OpenLink!");
+        LOGGER.info("\n   ____                       _       _         _    \n" +
+                "  / __ \\                     | |     (_)       | |   \n" +
+                " | |  | | _ __    ___  _ __  | |      _  _ __  | | __\n" +
+                " | |  | || '_ \\  / _ \\| '_ \\ | |     | || '_ \\ | |/ /\n" +
+                " | |__| || |_) ||  __/| | | || |____ | || | | ||   < \n" +
+                "  \\____/ | .__/  \\___||_| |_||______||_||_| |_||_|\\_\\\n" +
+                "         | |                                         \n" +
+                "         |_|                                         ");
+
     }
 }
