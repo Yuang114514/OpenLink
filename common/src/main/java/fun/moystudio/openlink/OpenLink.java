e2871ac7b92128dc3f57bf24d8323ab1a16e7f95
Terry-mcgit <13884967603@163.com>
2024-09-28 13:15:59 +0800

    Clear

--- common/src/main/java/fun/moystudio/openlink/OpenLink.java
+++
@@ -1,9 +0,0 @@
-package fun.moystudio.openlink;
-
-public final class OpenLink {
-    public static final String MOD_ID = "openlink";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
