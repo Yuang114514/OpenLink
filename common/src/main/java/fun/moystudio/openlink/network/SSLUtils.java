3d5fe34fe57a91d1f90d172baea6da12f688c32f
zirranYa <16774996@qq.com>
2024-11-16 00:20:08 +0800

    按原代码思路重写ssl禁用

--- common/src/main/java/fun/moystudio/openlink/network/SSLUtils.java
+++ common/src/main/java/fun/moystudio/openlink/network/SSLUtils.java
@@ -2,59 +2,53 @@ package fun.moystudio.openlink.network;
 
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
- 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.*;
+
 
 /**
-    @author https://www.cnblogs.com/xjbBill
-    如有侵权，请联系本mod作者删除
+    @author zirranYa
  **/
 
-public class SSLUtils {
-    public static boolean SSLIgnored=false;
 
-    private static void trustAllHttpsCertificates() throws Exception {
-        TrustManager[] trustAllCerts = new TrustManager[1];
-        TrustManager tm = new miTM();
-        trustAllCerts[0] = tm;
+public class SSLUtils {
+
+    private boolean sslIgnored = false;
+
+    private void trustAllHttpsCertificates() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[]{new miTM()};
         SSLContext sc = SSLContext.getInstance("SSL");
         sc.init(null, trustAllCerts, null);
         HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
- 
-    static class miTM implements TrustManager,X509TrustManager {
+
+    private static class miTM implements TrustManager, X509TrustManager {
         public X509Certificate[] getAcceptedIssuers() {
-            return null;
+            return new X509Certificate[0];
         }
- 
+
         public boolean isServerTrusted(X509Certificate[] certs) {
             return true;
         }
- 
+
         public boolean isClientTrusted(X509Certificate[] certs) {
             return true;
         }
- 
+
         public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
         }
- 
+
         public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
         }
     }
 
-    public static void ignoreSsl() throws Exception{
-        SSLIgnored=true;
-        HostnameVerifier hv = new HostnameVerifier() {
-            public boolean verify(String urlHostName, SSLSession session) {
-                return true;
-            }
-        };
+    public void ignoreSsl() throws Exception {
+        sslIgnored = true;
+        HostnameVerifier hv = (urlHostName, session) -> true;
         trustAllHttpsCertificates();
         HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    }
+
+    public boolean isSslIgnored() {
+        return sslIgnored;
     }
 }
