fe484e962da82c484f425bf25df57f760e176e34
Terry-mcgit <13884967603@163.com>
2024-12-07 13:59:45 +0800

    Ack -> Settings

--- common/src/main/java/fun/moystudio/openlink/gui/SettingScreen.java
+++ common/src/main/java/fun/moystudio/openlink/gui/SettingScreen.java
@@ -38,7 +38,7 @@ public class SettingScreen extends Screen {
     Screen lastscreen;
     SettingTabs tab=SettingTabs.USER;
     SettingTabs lasttab=null;
-    SettingScreenButton buttonLog, buttonInfo,buttonUser, buttonAck;
+    SettingScreenButton buttonLog,buttonInfo,buttonUser,buttonSetting;
     JsonResponseWithData<JsonUserInfo> userInfo=null;
     List<Widget> renderableTabWidgets,tabLog=new ArrayList<>(),tabInfo=new ArrayList<>(),tabUser=new ArrayList<>(),tabLogin_User=new ArrayList<>(),tabAck=new ArrayList<>();
 
@@ -56,11 +56,11 @@ public class SettingScreen extends Screen {
         buttonUser=new SettingScreenButton(5,40,i,20,SettingTabs.USER.component,(button -> tab=SettingTabs.USER));
         buttonLog=new SettingScreenButton(5+i,40,i,20,SettingTabs.LOG.component,(button -> tab=SettingTabs.LOG));
         buttonInfo=new SettingScreenButton(5+i*2,40,i,20,SettingTabs.INFO.component,(button -> tab=SettingTabs.INFO));
-        buttonAck=new SettingScreenButton(5+i*3,40,i,20,SettingTabs.ACKLIST.component,(button -> tab=SettingTabs.ACKLIST));
+        buttonSetting=new SettingScreenButton(5+i*3,40,i,20,SettingTabs.SETTING.component,(button -> tab=SettingTabs.SETTING));
         addRenderableWidget(buttonLog);
         addRenderableWidget(buttonInfo);
         addRenderableWidget(buttonUser);
-        addRenderableWidget(buttonAck);
+        addRenderableWidget(buttonSetting);
         //Temp variables
         ResourceLocation lastlocationimage=!tabUser.isEmpty()?((ImageWidget)tabUser.get(0)).texture:new ResourceLocation("openlink","textures/gui/default_avatar.png");
         Component lastcomponent1=tabUser.size()>=2?((ComponentWidget)tabUser.get(1)).component:TextComponent.EMPTY;
@@ -218,7 +218,7 @@ public class SettingScreen extends Screen {
         RenderSystem.setShaderColor(1.0F,1.0F,1.0F,1.0F);
         RenderSystem.setShaderTexture(0,BACKGROUND_SETTING);
         blit(poseStack,0,0,0,0,this.width,this.height,this.width,this.height);
-        fill(poseStack,5,60,this.buttonAck.x+this.buttonAck.getWidth(),this.height-5,0x8F000000);
+        fill(poseStack,5,60,this.buttonSetting.x+this.buttonSetting.getWidth(),this.height-5,0x8F000000);
         title.renderCentered(poseStack,this.width/2,15);
         if(renderableTabWidgets!=null) renderableTabWidgets.forEach(widget -> widget.render(poseStack,i,j,f));
         super.render(poseStack,i,j,f);
@@ -232,22 +232,22 @@ public class SettingScreen extends Screen {
                 buttonLog.active=false;
                 buttonInfo.active=true;
                 buttonUser.active=true;
-                buttonAck.active=true;
+                buttonSetting.active=true;
 
                 renderableTabWidgets=tabLog;
             }
-            case ACKLIST -> {
+            case SETTING -> {
                 buttonLog.active=true;
                 buttonInfo.active=true;
                 buttonUser.active=true;
-                buttonAck.active=false;
+                buttonSetting.active=false;
                 renderableTabWidgets=tabAck;
             }
             case USER -> {
                 buttonLog.active=true;
                 buttonInfo.active=true;
                 buttonUser.active=false;
-                buttonAck.active=true;
+                buttonSetting.active=true;
                 if(Request.Authorization==null){
                     renderableTabWidgets=tabLogin_User;
                     return;
@@ -309,7 +309,7 @@ public class SettingScreen extends Screen {
                 buttonLog.active=true;
                 buttonInfo.active=false;
                 buttonUser.active=true;
-                buttonAck.active=true;
+                buttonSetting.active=true;
                 renderableTabWidgets=tabInfo;
             }
         }
--- common/src/main/java/fun/moystudio/openlink/logic/SettingTabs.java
+++ common/src/main/java/fun/moystudio/openlink/logic/SettingTabs.java
@@ -7,7 +7,7 @@ public enum SettingTabs {
     LOG(new TranslatableComponent("tab.openlink.setting_log")),//隧道日志
     USER(new TranslatableComponent("tab.openlink.setting_user")),//用户信息
     INFO(new TranslatableComponent("tab.openlink.setting_info")),//相关信息
-    ACKLIST(new TranslatableComponent("tab.openlink.setting_acklist"));//鸣谢名单
+    SETTING(new TranslatableComponent("tab.openlink.setting_setting"));//设置
     public final Component component;
     SettingTabs(Component component1){
         component=component1;
