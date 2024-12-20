e4768fc6540315b0c587b4c583793a54ab5c6f19
Terry-mcgit <13884967603@163.com>
2024-12-20 19:37:16 +0800

    setting image button changed

---
+++ common/src/main/java/fun/moystudio/openlink/gui/ImageButtonWithHoveredState.java
@@ -0,0 +1,45 @@
+package fun.moystudio.openlink.gui;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
+
+public class ImageButtonWithHoveredState extends ImageButton {
+    private ResourceLocation resourceLocation;
+    private int yTexStart;
+    private int xTexStart;
+    private int yDiffTex;
+    private int textureWidth;
+    private int textureHeight;
+    private ResourceLocation resourceLocationHovered;
+
+    public ImageButtonWithHoveredState(int i, int j, int k, int l, int m, int n, int o, ResourceLocation resourceLocation, ResourceLocation rl2, int p, int q, OnPress onPress) {
+        super(i, j, k, l, m, n, o, resourceLocation, p, q, onPress);
+        this.textureWidth = p;
+        this.textureHeight = q;
+        this.xTexStart = m;
+        this.yTexStart = n;
+        this.yDiffTex = o;
+        this.resourceLocation = resourceLocation;
+        this.resourceLocationHovered = rl2;
+    }
+
+    @Override
+    public void renderButton(PoseStack poseStack, int i, int j, float f) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, this.resourceLocation);
+        int k = this.yTexStart;
+        if (this.isHoveredOrFocused()) {
+            k += this.yDiffTex;
+            RenderSystem.setShaderTexture(0, this.resourceLocationHovered);
+        }
+
+        RenderSystem.enableDepthTest();
+        blit(poseStack, this.x, this.y, (float)this.xTexStart, (float)k, this.width, this.height, this.textureWidth, this.textureHeight);
+        if (this.isHovered) {
+            this.renderToolTip(poseStack, i, j);
+        }
+    }
+}
--- common/src/main/java/fun/moystudio/openlink/mixin/ShareToLanScreenMixin.java
+++ common/src/main/java/fun/moystudio/openlink/mixin/ShareToLanScreenMixin.java
@@ -2,7 +2,7 @@ package fun.moystudio.openlink.mixin;
 
 import fun.moystudio.openlink.OpenLink;
 import fun.moystudio.openlink.frpc.Frpc;
-import fun.moystudio.openlink.gui.SettingButton;
+import fun.moystudio.openlink.gui.ImageButtonWithHoveredState;
 import fun.moystudio.openlink.gui.SettingScreen;
 import fun.moystudio.openlink.logic.LanConfig;
 import fun.moystudio.openlink.logic.OnlineModeTabs;
@@ -16,7 +16,6 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.ShareToLanScreen;
 import net.minecraft.network.chat.*;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.util.HttpUtil;
 import net.minecraft.world.level.GameType;
 import org.spongepowered.asm.mixin.Mixin;
@@ -35,6 +34,7 @@ import java.util.List;
 public abstract class ShareToLanScreenMixin extends Screen{
     @Unique
     private static final ResourceLocation OPENLINK_SETTING = new ResourceLocation("openlink", "textures/gui/setting.png");
+    private static final ResourceLocation OPENLINK_SETTING_HOVERED = new ResourceLocation("openlink", "textures/gui/setting_hovered.png");
 
     @Shadow private GameType gameMode;
 
@@ -74,8 +74,8 @@ public abstract class ShareToLanScreenMixin extends Screen{
         this.addRenderableWidget(usingfrp);
         this.addRenderableWidget(onlinemode);
         this.addRenderableWidget(allowpvp);
-        this.addRenderableWidget(new SettingButton(this.width / 2 + 5+150+10, this.height - 28,
-                20, 20, 0, 0, 20, OPENLINK_SETTING, 20, 20, (button) -> {
+        this.addRenderableWidget(new ImageButtonWithHoveredState(this.width / 2 + 129, this.height / 4 + 48 + 72 + 12,
+                20, 20, 0, 0, 20, OPENLINK_SETTING, OPENLINK_SETTING_HOVERED, 20, 20, (button) -> {
             this.minecraft.setScreen(new SettingScreen(this));
         }));
     }
