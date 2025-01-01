319ea57fe0f6029bffaeaf3df9836d1e2e45ab0c
Terry-mcgit <13884967603@163.com>
2025-01-01 14:27:35 +0800

    1.0.0-1.19.2

--- fabric-like/src/main/java/fun/moystudio/openlink/fabriclike/OpenLinkFabricLike.java
+++ fabric-like/src/main/java/fun/moystudio/openlink/fabriclike/OpenLinkFabricLike.java
@@ -3,7 +3,7 @@ package fun.moystudio.openlink.fabriclike;
 import fun.moystudio.openlink.OpenLink;
 import fun.moystudio.openlink.frpc.Frpc;
 import fun.moystudio.openlink.logic.EventCallbacks;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
@@ -13,7 +13,7 @@ public final class OpenLinkFabricLike {
     public static void init(String version,String loader,String loader_version) throws Exception {
         // Run our common setup.
         OpenLink.init(version,loader,loader_version);
-        ClientCommandManager.DISPATCHER.register(ClientCommandManager
+        ClientCommandManager.getActiveDispatcher().register(ClientCommandManager
                         .literal("proxyrestart")
                         .executes(context -> Frpc.openFrp(Minecraft.getInstance().getSingleplayerServer().getPort(),"")?1:0));
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight)->{
