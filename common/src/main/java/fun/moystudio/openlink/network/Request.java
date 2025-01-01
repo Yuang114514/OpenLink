373be77b663c3a4cded8cb15bdb26d221cdb52b9
Terry-mcgit <13884967603@163.com>
2025-01-01 14:24:52 +0800

    1.0.0-1.19.2

--- common/src/main/java/fun/moystudio/openlink/network/Request.java
+++ common/src/main/java/fun/moystudio/openlink/network/Request.java
@@ -13,8 +13,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.prefs.Preferences;
 
 public class Request {
     public static String Authorization=null;
