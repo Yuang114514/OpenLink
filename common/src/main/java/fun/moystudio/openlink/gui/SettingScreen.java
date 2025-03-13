aa633c6a704367de0fb64d2fd6d8dbfd11a3f41e
Terry-MC123 <m13884967603@163.com>
2025-03-13 22:17:17 +0800

    migrate

--- common/src/main/java/fun/moystudio/openlink/gui/SettingScreen.java
+++ common/src/main/java/fun/moystudio/openlink/gui/SettingScreen.java
@@ -141,10 +141,10 @@ public class SettingScreen extends Screen {
             sensitiveInfoHiding = object;
             OpenLink.PREFERENCES.putBoolean("setting_sensitive_info_hiding", object);
         }));
-        tabSetting.add(new Button(this.width/2-75,65+70,150,20,Utils.translatableText("text.openlink.ofpanel"),button -> {
+        tabSetting.add(Button.builder(Utils.translatableText("text.openlink.ofpanel"),button -> {
             this.minecraft.keyboardHandler.setClipboard("https://console.openfrp.net/fastlogin?auth="+Request.Authorization);
             new WebBrowser("https://console.openfrp.net/fastlogin?auth="+Request.Authorization).openBrowser();
-        }));
+        }).bounds(this.width/2-75,65+70,150,20).build());
         tabSetting.add(new ComponentWidget(this.font,this.width/2,this.height/2,0xffffff, Utils.translatableText("temp.openlink.tobedone"),true));
     }
 
